fa8500686487e75e58f009cbb10bf81aa69fdee1
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-05 09:46:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import CartContainer from './components/CartContainer';
 import { useDispatch, useSelector } from 'react-redux';
 import { calculateTotals } from './features/cart/cartSlice';
 import { useEffect } from 'react';
+import Modal from './components/Modal';
 
 function App() {
   const { cartItems } = useSelector(store => store.cart);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
+    // eslint-disable-next-line
   }, [cartItems]);
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
